1ce6acf753b51cad9d389f3dc73489fd71f0e34c
Jure Gregorin <jure.gregorin@blackstormlabs.com>
2019-09-19 23:16:53 +0200

    Fixes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+DOTS_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-cp -rT $PATH/data $HOME
+cp -rT $DOTS_PATH/data $HOME
